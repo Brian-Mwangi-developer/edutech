cd279e0daf676885a4e5d85f793bacd74134e9f0
Brian Mwangi <70840946+Brian-Mwangi-developer@users.noreply.github.com>
2025-03-20 09:50:04 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button";
+
+
+export default async function Home() {
+
+  return (
+  <div>
+    Hello New User
+    <Button>Click me</Button>
+  </div>
+  );
+}
